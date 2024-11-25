af32621b608b5fb005ab77c4c6064b198ed7a6c2
RandomStuff45 <luca.s.salazar@hotmail.com>
2024-11-25 12:38:05 -0500

    Created docs using sphinx and readthedocs

---
+++ docs/conf.py
@@ -0,0 +1,32 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'Qfiquiti'
+copyright = '2024, Luca Salazar'
+author = 'Luca Salazar'
+release = '11/23/2024'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    'sphinx.ext.autodoc',       # For docstrings
+    'sphinx.ext.viewcode',      # View source code links
+    'sphinx.ext.napoleon',      # Google-style docstrings
+]
+
+templates_path = ['_templates']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'sphinx_rtd_theme'
+html_static_path = ['_static']
